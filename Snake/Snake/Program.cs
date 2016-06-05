a3c80d4fff4916cb01540ad4ccc2400e7d256835
Alexey Shein <strategdzr@gmail.com>
2016-06-05 20:39:12 +0300

    Draw

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,9 +10,27 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int x1 = 5;
+            int y1 = 10;
+            char sym1 = '*';
+
+            Draw(x1, y1, sym1);
+
+            int x2 = 2;
+            int y2 = 8;
+            char sym2 = '/';
+
+            Draw(x2, y2, sym2);
+
+
             Console.ReadLine();
 
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
