1ac03118152de595ca09ff96fbac37310f2433e3
Alexey Shein <strategdzr@gmail.com>
2016-06-08 20:37:32 +0300

    List

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -17,6 +17,17 @@ namespace Snake
             Point p2 = new Point(3, 5, '/');
             p2.Draw();
 
+            Point p3 = new Point(7, 8, '#');
+            p3.Draw();
+
+            Point p4 = new Point(0, 2, '+');
+            p4.Draw();
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
 
             Console.ReadLine();
         }
